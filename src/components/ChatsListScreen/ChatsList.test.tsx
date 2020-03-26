79dafa148fa4782781af45cad364e0bdda2aead8
Cedric Nicoloso <cedric.nicoloso@gmail.com>
2020-03-27 00:47:06 +0100

    Add click test

--- src/components/ChatsListScreen/ChatsList.test.tsx
+++ src/components/ChatsListScreen/ChatsList.test.tsx
@@ -1,10 +1,23 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
-import { act, cleanup, render, waitFor } from '@testing-library/react'
+import {
+  act,
+  cleanup,
+  render,
+  fireEvent,
+  waitFor,
+} from '@testing-library/react'
+import { createBrowserHistory } from 'history'
 import ChatsList from './ChatsList'
 
 describe('ChatsList', () => {
-  afterEach(cleanup)
+  afterEach(() => {
+    cleanup()
+    delete window.location
+
+    window.location = {
+      href: '/',
+    }
+  })
 
   it('renders fetched chats data', async () => {
     fetchMock.mockResponseOnce(
@@ -26,8 +39,10 @@ describe('ChatsList', () => {
       })
     )
 
+    const history = createBrowserHistory()
+
     await act(async () => {
-      const { getByTestId } = render(<ChatsList />)
+      const { getByTestId } = render(<ChatsList history={history} />)
 
       await waitFor(() => expect(fetchMock).toHaveBeenCalledTimes(1))
 
@@ -39,5 +54,38 @@ describe('ChatsList', () => {
       expect(getByTestId('content')).toHaveTextContent('Hello')
       expect(getByTestId('date')).toHaveTextContent('01:00')
     })
+  })
+
+  it('should navigate to the target chat room on chat item click', async () => {
+    fetchMock.mockResponseOnce(
+      JSON.stringify({
+        data: {
+          chats: [
+            {
+              id: 1,
+              name: 'Foo Bar',
+              picture: 'https://localhost:4000/picture.jpg',
+              lastMessage: {
+                id: 1,
+                content: 'Hello',
+                createdAt: new Date('1 Jan 2019 GMT'),
+              },
+            },
+          ],
+        },
+      })
+    )
+
+    const history = createBrowserHistory()
+
+    {
+      const { getByTestId } = render(<ChatsList history={history} />)
+
+      await waitFor(() => expect(fetchMock).toHaveBeenCalledTimes(2))
+
+      fireEvent.click(getByTestId('chat'))
+
+      await waitFor(() => expect(history.location.pathname).toEqual('/chats/1'))
+    }
   })
 })

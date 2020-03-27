d3c8161bab9c54c367c6a4d3a13d722c022af8b2
Cedric Nicoloso <cedric.nicoloso@gmail.com>
2020-03-27 19:03:15 +0100

    Fix all chats query...

--- src/graphql/queries/chats.query.ts
+++ src/graphql/queries/chats.query.ts
@@ -2,8 +2,8 @@ import gql from 'graphql-tag'
 import * as fragments from '../fragments'
 
 export default gql`
-  query GetChat($chatId: ID!) {
-    chat(chatId: $chatId) {
+  query GetChats {
+    chats {
       ...Chat
     }
   }

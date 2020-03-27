93a66381eb59ed11718b05feb3e657a4d6d5a498
Cedric Nicoloso <cedric.nicoloso@gmail.com>
2020-03-27 01:24:08 +0100

    Configure Apollo client

--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,24 @@
 import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles'
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { ApolloProvider } from '@apollo/react-hooks'
 import './index.css'
 import App from './App'
 import * as serviceWorker from './serviceWorker'
+import client from './client'
 
 const theme = createMuiTheme({
   palette: {
     primary: { main: '#2c6157' },
     secondary: { main: '#6fd056' },
   },
-});
+})
 
 ReactDOM.render(
   <MuiThemeProvider theme={theme}>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </MuiThemeProvider>,
   document.getElementById('root')
 )

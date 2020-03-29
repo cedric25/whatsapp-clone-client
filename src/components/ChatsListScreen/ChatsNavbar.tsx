579102ca8a208ca83bbc58e3401a87760adcd393
Cedric Nicoloso <cedric.nicoloso@gmail.com>
2020-03-29 17:42:09 +0200

    Quick signout button

--- src/components/ChatsListScreen/ChatsNavbar.tsx
+++ src/components/ChatsListScreen/ChatsNavbar.tsx
@@ -1,14 +1,47 @@
-import React from 'react'
-import { Toolbar } from '@material-ui/core';
-import styled from 'styled-components';
+import React, { useCallback } from 'react'
+import { History } from 'history'
+import { Button, Toolbar } from '@material-ui/core'
+import SignOutIcon from '@material-ui/icons/PowerSettingsNew'
+import styled from 'styled-components'
+import { useSignOut } from '../../services/auth.service'
 
 const Container = styled(Toolbar)`
+  display: flex;
   background-color: var(--primary-bg);
   color: var(--primary-text);
   font-size: 20px;
   line-height: 40px;
-`;
+`
 
-const ChatsNavBar: React.FC = () => <Container>WhatsApp Clone</Container>
+const Title = styled.div`
+  flex: 1;
+`
 
-export default ChatsNavBar
+const LogoutButton = styled(Button)`
+  color: var(--primary-text) !important;
+`
+
+interface ChildComponentProps {
+  history: History
+}
+
+const ChatsNavbar: React.FC<ChildComponentProps> = ({ history }) => {
+  const signOut = useSignOut()
+
+  const handleSignOut = useCallback(() => {
+    signOut().then(() => {
+      history.replace('/sign-in')
+    })
+  }, [history, signOut])
+
+  return (
+    <Container>
+      <Title>WhatsApp Clone</Title>
+      <LogoutButton data-testid="sign-out-button" onClick={handleSignOut}>
+        <SignOutIcon />
+      </LogoutButton>
+    </Container>
+  )
+}
+
+export default ChatsNavbar

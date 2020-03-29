bc97fe93ca0a9703dfac2967128bfd293a09c31a
Cedric Nicoloso <cedric.nicoloso@gmail.com>
2020-03-29 15:12:14 +0200

    Use subscriptions on the client

--- src/components/ChatRoomScreen/index.tsx
+++ src/components/ChatRoomScreen/index.tsx
@@ -1,15 +1,10 @@
 import React, { useCallback } from 'react'
 import styled from 'styled-components'
 import { History } from 'history'
-import { defaultDataIdFromObject } from 'apollo-cache-inmemory'
 import gql from 'graphql-tag'
-import {
-  GetChatsQuery,
-  useGetChatQuery,
-  useAddMessageMutation,
-} from '../../graphql/types'
-import * as queries from '../../graphql/queries'
+import { useGetChatQuery, useAddMessageMutation } from '../../graphql/types'
 import * as fragments from '../../graphql/fragments'
+import { writeMessage } from '../../services/cache.service'
 
 // Components
 import ChatNavbar from './ChatNavbar'
@@ -48,10 +43,6 @@ interface ChatRoomScreenParams {
   history: History
 }
 
-interface ChatsResult {
-  chats: any[]
-}
-
 const ChatRoomScreen: React.FC<ChatRoomScreenParams> = ({
   history,
   chatId,
@@ -80,79 +71,16 @@ const ChatRoomScreen: React.FC<ChatRoomScreenParams> = ({
             __typename: 'Message',
             id: Math.random().toString(36).substr(2, 9),
             createdAt: new Date(),
+            chat: {
+              __typename: 'Chat',
+              id: chatId,
+            },
             content,
           },
         },
         update: (client, { data }) => {
           if (data && data.addMessage) {
-            type FullChat = { [key: string]: any }
-            let fullChat
-            const chatIdFromStore = defaultDataIdFromObject(chat)
-
-            if (chatIdFromStore === null) {
-              return
-            }
-
-            try {
-              fullChat = client.readFragment<FullChat>({
-                id: chatIdFromStore,
-                fragment: fragments.fullChat,
-                fragmentName: 'FullChat',
-              })
-            } catch (e) {
-              return
-            }
-
-            if (fullChat === null || fullChat.messages === null) {
-              return
-            }
-            if (
-              fullChat.messages.some(
-                (currentMessage: any) =>
-                  data.addMessage && currentMessage.id === data.addMessage.id
-              )
-            ) {
-              return
-            }
-
-            fullChat.messages.push(data.addMessage)
-            fullChat.lastMessage = data.addMessage
-
-            client.writeFragment({
-              id: chatIdFromStore,
-              fragment: fragments.fullChat,
-              fragmentName: 'FullChat',
-              data: fullChat,
-            })
-
-            let clientChatsData: GetChatsQuery | null
-            try {
-              clientChatsData = client.readQuery({
-                query: queries.chats,
-              })
-            } catch (e) {
-              return
-            }
-
-            if (!clientChatsData || !clientChatsData.chats) {
-              return null
-            }
-            const chats = clientChatsData.chats
-
-            const chatIndex = chats.findIndex(
-              (currentChat: any) => currentChat.id === chatId
-            )
-            if (chatIndex === -1) return
-            const chatWhereAdded = chats[chatIndex]
-
-            // The chat will appear at the top of the ChatsList component
-            chats.splice(chatIndex, 1)
-            chats.unshift(chatWhereAdded)
-
-            client.writeQuery({
-              query: queries.chats,
-              data: { chats: chats },
-            })
+            writeMessage(client, data.addMessage)
           }
         },
       })
--- src/components/ChatsListScreen/ChatsList.test.tsx
+++ src/components/ChatsListScreen/ChatsList.test.tsx
@@ -40,6 +40,10 @@ describe('ChatsList', () => {
                   id: 1,
                   content: 'Hello',
                   createdAt: new Date('1 Jan 2019 GMT'),
+                  chat: {
+                    __typename: 'Chat',
+                    id: 1,
+                  },
                 },
               },
             ],
@@ -84,6 +88,10 @@ describe('ChatsList', () => {
                   id: 1,
                   content: 'Hello',
                   createdAt: new Date('1 Jan 2019 GMT'),
+                  chat: {
+                    __typename: 'Chat',
+                    id: 1,
+                  },
                 },
               },
             ],
